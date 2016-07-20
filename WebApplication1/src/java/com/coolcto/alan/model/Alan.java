84573e96001272d0539792d05e0c68003ec01376
ShabbyLiu <614435388@qq.com>
2016-07-20 23:46:51 +0800

    都是垃圾

---
+++ WebApplication1/src/java/com/coolcto/alan/model/Alan.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.coolcto.alan.model;
+
+/**
+ *
+ * @author liu
+ */
+public class Alan extends Man{
+    public Alan(){
+//         int age;
+//    int height;
+//    String name;
+        this.age=20;
+        this.height=170;
+        this.name="alan";
+
+    }
+    
+}
